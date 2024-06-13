60dfc891b3957ff3fbef0f440b408d3878df4834
Billie Thompson <billie.thompson@armakuni.com>
2024-06-13 11:47:47 +0200

    Use alternative syntax for loading database url

--- src/datastore.py
+++ src/datastore.py
@@ -3,8 +3,9 @@ import os
 from sqlalchemy import create_engine
 
 DATABASE_URL = (
-    os.environ.get("DATABASE_URL")
-    or "postgresql://postgres:scoopdash@localhost:5433/postgres"
+    os.environ["DATABASE_URL"]
+    if "DATABASE_URL" in os.environ
+    else "postgresql://postgres:scoopdash@localhost:5433/postgres"
 )
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
