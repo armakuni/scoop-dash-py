96b5fc335ba28d8d95b6a614e15058d0b96472b5
Billie Thompson <billie.thompson@armakuni.com>
2024-06-13 11:50:43 +0200

    Follow ruff advice

--- src/datastore.py
+++ src/datastore.py
@@ -2,10 +2,8 @@ import os
 
 from sqlalchemy import create_engine
 
-DATABASE_URL = (
-    os.environ["DATABASE_URL"]
-    if "DATABASE_URL" in os.environ
-    else "postgresql://postgres:scoopdash@localhost:5433/postgres"
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL", "postgresql://postgres:scoopdash@localhost:5433/postgres"
 )
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
