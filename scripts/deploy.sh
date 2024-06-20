c6e357e6b66ab35b34f193ec56319b0aac25845b
VIkalpSimform <vikalp@simformsolutions.com>
2024-06-20 16:46:33 +0530

    added failure code

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,6 +29,7 @@ check_deployment_status() {
 check_health() {
 	echo "Checking health status..."
 	curl --fail -L https://scoop-dash-py.onrender.com/hello/v1 -o /dev/null 
+	
 }
 
 # Loop until the deployment status is "live"
